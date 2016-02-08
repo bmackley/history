8f4cd047f5b7db383dbdb69f32113c44e250b65b
Ben Mackley <benjaminmackley@gmail.com>
2016-02-08 16:29:51 -0700

    hello there

---
+++ homePage/views/create_a_char.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.contrib.auth import authenticate, login
+from homePage import models as m
+from . import templater
+import glob
+
+def process_request(request):
+ 
+
+  tvars = {
+  }
+  return templater.render_to_response(request, 'login.html', tvars)
